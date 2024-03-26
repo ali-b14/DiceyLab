75f5b48fd08b8b6a42e30b5493320eb621eb9f23
ali-b14 <alib714@gmail.com>
2024-03-26 12:34:17 -0400

    Started Dice and Bins class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,32 @@
 public class Dice {
+    private Dice craps = new Dice(2);
+    private Dice yatzee = new Dice(5);
+    private Integer toss;
 
+    public Dice() {
+    }
 
+    public Dice getCraps() {
+        return craps;
+    }
+
+    public void setCraps(Dice craps) {
+        this.craps = craps;
+    }
+
+    public Dice getYatzee() {
+        return yatzee;
+    }
+
+    public void setYatzee(Dice yatzee) {
+        this.yatzee = yatzee;
+    }
+
+    public Integer getToss() {
+        return toss;
+    }
+
+    public void setToss(Integer toss) {
+        this.toss = toss;
+    }
 }
