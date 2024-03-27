083c37460e3c6bdff4e551092308e995a8117979
ali-b14 <alib714@gmail.com>
2024-03-27 18:00:54 -0400

    Updated Bins and Simulation

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,105 +1,21 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Bins {
-    private Integer bin2;
-    private Integer bin3;
-    private Integer bin4;
-    private Integer bin5;
-    private Integer bin6;
-    private Integer bin7;
-    private Integer bin8;
-    private Integer bin9;
-    private Integer bin10;
-    private Integer bin11;
-    private Integer bin12;
+    private Map<Integer, Integer> bins = new HashMap<>();
 
-    public Bins(Integer bin2, Integer... bin) {
+
+    public Bins(Map bins) {
+        this.bins = bins;
+    }
+    public Map<Integer, Integer> getBins() {
+        return bins;
     }
 
-    public Integer getBin2() {
-        return bin2;
+    public void setBins(Map<Integer, Integer> bins) {
+        this.bins = bins;
     }
 
-    public void setBin2(Integer bin2) {
-        this.bin2 = bin2;
-    }
-
-    public Integer getBin3() {
-        return bin3;
-    }
-
-    public void setBin3(Integer bin3) {
-        this.bin3 = bin3;
-    }
-
-    public Integer getBin4() {
-        return bin4;
-    }
-
-    public void setBin4(Integer bin4) {
-        this.bin4 = bin4;
-    }
-
-    public Integer getBin5() {
-        return bin5;
-    }
-
-    public void setBin5(Integer bin5) {
-        this.bin5 = bin5;
-    }
-
-    public Integer getBin6() {
-        return bin6;
-    }
-
-    public void setBin6(Integer bin6) {
-        this.bin6 = bin6;
-    }
-
-    public Integer getBin7() {
-        return bin7;
-    }
-
-    public void setBin7(Integer bin7) {
-        this.bin7 = bin7;
-    }
-
-    public Integer getBin8() {
-        return bin8;
-    }
-
-    public void setBin8(Integer bin8) {
-        this.bin8 = bin8;
-    }
-
-    public Integer getBin9() {
-        return bin9;
-    }
-
-    public void setBin9(Integer bin9) {
-        this.bin9 = bin9;
-    }
-
-    public Integer getBin10() {
-        return bin10;
-    }
-
-    public void setBin10(Integer bin10) {
-        this.bin10 = bin10;
-    }
-
-    public Integer getBin11() {
-        return bin11;
-    }
-
-    public void setBin11(Integer bin11) {
-        this.bin11 = bin11;
-    }
-
-    public Integer getBin12() {
-        return bin12;
-    }
-
-    public void setBin12(Integer bin12) {
-        this.bin12 = bin12;
-    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,11 @@
 public class Simulation {
+    private Integer numberOfDies;
+    private Integer numberOfTosses;
 
+    Bins bin = new Bins();//How should create the bin object so I can save the results into it? Would I be able to directly save the results into the bins class
 
-
+    public Simulation(Integer numberOfDies, Integer numberOfTosses) {
+        this.numberOfDies = numberOfDies;
+        this.numberOfTosses = numberOfTosses;
+    }
 }
