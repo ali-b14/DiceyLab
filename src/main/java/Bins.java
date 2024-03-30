a207ffd3f92de74a9274da618a97487d66447fd0
ali-b14 <alib714@gmail.com>
2024-03-30 11:02:31 -0400

    Updated dice game logic

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -4,22 +4,21 @@ import java.util.List;
 import java.util.Map;
 
 public class Bins {
-//    private Map<Integer, Integer> bins = new HashMap<>();
-//
-//
-//    public Bins(Map bins) {
-//        this.bins = bins;
-//    }
-//    public Map<Integer, Integer> getBins() {
-//        return bins;
-//    }
-//
-//    public void setBins(Map<Integer, Integer> bins) {
-//        this.bins = bins;
-//    }
-//
+    private Map<Integer, Integer> bins = new HashMap<>();
+
+
+    public Bins(Map bins) {
+        this.bins = bins;
+    }
+    public Map<Integer, Integer> getBins() {
+        return bins;
+    }
 //    public void updateValue(Integer key, Integer value){
-//        bins.put(bins.merge(bins, 1, Integer::sum));
+//        bins.put(bins.merge(bins.size(), 1, Integer::sum));
 //    }
+    public void updateValue(Integer key, Integer value){
+        bins.put(key, bins.merge(key, value, Integer::sum));
+    }
+
 
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -5,28 +5,52 @@ import java.util.Random;
 
 public class Dice {
 
-    private List<Integer> dices = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+    private final List<Integer> dice = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
     int toss;
+    int numberOfDice;
+    int sum = 0;
     Random random = new Random();
 
 
-    public Dice(int toss) {
+    public Dice(int numberOfDice,int toss) {
+        this.numberOfDice = numberOfDice;
         this.toss = toss;
     }
 
-//    public static void main(String[] args) {
-//        Dice dice = new Dice(2);
-//        System.out.println(dice.tossAndSum(2));
-//    }
+    public static void main(String[] args) {
+        Dice dice = new Dice(2, 3);
+        System.out.println(dice.tossAndSum(2,3));
+    }
 
-    public Integer tossAndSum(int toss){
-        Integer sum = 0;
-        Integer value;
+    public Integer tossAndSum(int numberOfDice, int toss) {
+
         for (int i = 0; i < toss; i++) {
-            value = dices.get(random.nextInt(dices.size()));
-            sum += value;
+            sum =0;
+            System.out.println("Toss number: " + (i + 1));
+            for (int j = 0; j < numberOfDice; j++) {
+                int value = dice.get(random.nextInt(dice.size()));
+                System.out.println("Dice value = " + value);
+                sum += value;
+                System.out.println("Sum: " + sum);
+            }
+            System.out.println(numberOfDice + " dice have been rolled and the sum = " + sum);
         }
         return sum;
     }
 
+    public List<Integer> getDice() {
+        return dice;
+    }
+
+    public int getToss() {
+        return toss;
+    }
+
+    public void setToss(int toss) {
+        this.toss = toss;
+    }
+
+    public int getSum() {
+        return sum;
+    }
 }
