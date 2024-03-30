7adf7805d332ff369aeca69c06b4efa11c615104
ali-b14 <alib714@gmail.com>
2024-03-30 07:51:21 -0400

    Dice class updated, can run rolling of 2 die.

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,32 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Dice {
-    private Dice craps = new Dice(2);
-    private Dice yatzee = new Dice(5);
-    private Integer toss;
+
+    private List<Integer> dices = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+    Random random = new Random();
+
 
     public Dice() {
     }
 
-    public Dice getCraps() {
-        return craps;
+//    public static void main(String[] args) {
+//        Dice dice = new Dice();
+//        System.out.println(dice.tossAndSum());
+//    }
+
+    public Integer tossAndSum(){
+        int toss = 2;
+        Integer sum = 0;
+        Integer value;
+        for (int i = 0; i < toss; i++) {
+            value = dices.get(random.nextInt(dices.size()));
+            sum += value;
+        }
+        return sum;
     }
 
-    public void setCraps(Dice craps) {
-        this.craps = craps;
-    }
-
-    public Dice getYatzee() {
-        return yatzee;
-    }
-
-    public void setYatzee(Dice yatzee) {
-        this.yatzee = yatzee;
-    }
-
-    public Integer getToss() {
-        return toss;
-    }
-
-    public void setToss(Integer toss) {
-        this.toss = toss;
-    }
 }
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,11 +1,25 @@
 public class Simulation {
-    private Integer numberOfDies;
-    private Integer numberOfTosses;
+    private Integer numberOfDies = 2; //throw 2 dies
+    private Integer numberOfTosses = 1000000; //throw 1 million times
 
-    Bins bin = new Bins();//How should create the bin object so I can save the results into it? Would I be able to directly save the results into the bins class
+//    Bins bin = new Bins();//How should create the bin object so I can save the results into it? Would I be able to directly save the results into the bins class
 
     public Simulation(Integer numberOfDies, Integer numberOfTosses) {
         this.numberOfDies = numberOfDies;
         this.numberOfTosses = numberOfTosses;
     }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void run(){
+
+    }
+
+    public Integer throwDice(){
+        return null;
+    }
+
+
 }
