facc5b59f7755b341e3db7bbe4f7f192e6fbfc18
ali-b14 <alib714@gmail.com>
2024-04-01 12:04:40 -0400

    Finished for now will come back when I have time to implement results into txt.file and formatting my results

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class Dice {
 
     private final List<Integer> dice = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
-    private int toss;
+    private int numberOfTosses;
     private int numberOfDice;
     private int sum = 0;
     Random random = new Random();
     private Bins bins;
     private Simulation simulation;
     Map<Integer,Integer> diceTracker = new HashMap<>();
-    public Dice(int numberOfDice,int toss) {
+    public Dice(int numberOfDice,int numberOfTosses) {
         this.numberOfDice = numberOfDice;
-        this.toss = toss;
+        this.numberOfTosses = numberOfTosses;
         this.bins = new Bins();//initialize Bins object
 //        this.simulation = new Simulation(2,10);
     }
@@ -40,11 +40,11 @@ public class Dice {
     public List<Integer> getDice() {
         return dice;
     }
-    public int getToss() {
-        return toss;
+    public int getNumberOfTosses() {
+        return numberOfTosses;
     }
-    public void setToss(int toss) {
-        this.toss = toss;
+    public void setNumberOfTosses(int numberOfTosses) {
+        this.numberOfTosses = numberOfTosses;
     }
     public int getSum() {
         return sum;
--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,20 +1,20 @@
-import java.util.Map;
-
 public class Simulation {
-    private int numberOfDies; //throw 2-5 dies
+    private int numberOfDice; //throw 2-5 dies
     private int numberOfTosses; //throw up to 1 million times
-    Bins bins = new Bins();
-    public Simulation(int numberOfDies, int numberOfTosses) {
-        this.numberOfDies = numberOfDies;
+    private Bins bins;
+
+    public Simulation(int numberOfDice, int numberOfTosses) {
+        this.numberOfDice = numberOfDice;
         this.numberOfTosses = numberOfTosses;
+        this.bins = new Bins();
     }
-    Dice dice = new Dice(2,5);
     public static void main(String[] args) {
-        Simulation simulation = new Simulation(2,5);
+        Simulation simulation = new Simulation(2,1000000);
         simulation.run();
     }
     public void run(){
-        dice.tossAndSum(2,1000000);
+        Dice dice = new Dice(numberOfDice,numberOfTosses);
+        dice.tossAndSum(numberOfDice,numberOfTosses);
     }
     public Integer throwDice(){
         return null;
